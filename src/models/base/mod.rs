3d29a2d06ad336ff4f8cec4be725ca00160c52cb
UlmerMan <manuel.ulmer@gmx.net>
2026-09-03 14:09:01 +0200

    Fix clippy warnings

--- src/models/base/mod.rs
+++ src/models/base/mod.rs
@@ -1,9 +1,9 @@
-pub mod base;
+pub mod base_reqs;
 pub mod collaborator;
 pub mod collaborator_type;
 pub mod order;
 pub mod role;
 pub mod template;
 
-pub use base::{Base, PostBaseRequest, UpdateBaseRequest, UpdateBaseResponse};
+pub use base_reqs::{Base, PostBaseRequest, UpdateBaseRequest, UpdateBaseResponse};
 pub use collaborator::GetCollaboratorsQuery;
